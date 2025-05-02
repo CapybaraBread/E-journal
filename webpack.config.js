3eb935cad7df2c6992003579a1a3197a8effd86b
NadyaEvp <t_evp@mail.ru>
2025-05-02 19:35:07 +0300

    Удаляю лишнее

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     target,
     devtool,
     entry: {
-        index: path.resolve(__dirname, 'a/grades.pug')
+        index: path.resolve(__dirname, 'pug/avtor.pug')
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
